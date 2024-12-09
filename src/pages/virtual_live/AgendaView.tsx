ce0afc8c724174b88b328cf96c62c41e63dec0b8
dnaroma <me@dna.moe>
2024-12-09 20:42:37 +0100

    feat(AgendaView): enhance layout with AgendaBox and AgendaPaper components

--- src/pages/virtual_live/AgendaView.tsx
+++ src/pages/virtual_live/AgendaView.tsx
@@ -2,7 +2,6 @@ import { Typography, Grid, Container, Paper } from "@mui/material";
 import { Skeleton } from "@mui/material";
 import React, { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
-import { Link } from "react-router-dom";
 import { IVirtualLiveInfo } from "../../types.d";
 import { getRemoteAssetURL } from "../../utils";
 import { useAssetI18n } from "../../utils/i18n";
@@ -12,6 +11,8 @@ import Image from "mui-image";
 import { observer } from "mobx-react-lite";
 import { useRootStore } from "../../stores/root";
 import SpoilerCard from "../../components/helpers/SpoilerCard";
+import AgendaBox from "../../components/styled/AgendaBox";
+import AgendaPaper from "../../components/styled/AgendaPaper";
 
 const AgendaView: React.FC<{ data?: IVirtualLiveInfo }> = observer(
   ({ data }) => {
@@ -67,91 +68,87 @@ const AgendaView: React.FC<{ data?: IVirtualLiveInfo }> = observer(
       );
     }
     return (
-      <Link to={"/virtual_live/" + data.id} style={{ textDecoration: "none" }}>
-        <Container maxWidth="md" sx={{ cursor: "pointer" }}>
-          <SpoilerCard
-            releaseTime={data.startAt}
-            toPath={"/virtual_live/" + data.id}
+      <SpoilerCard
+        releaseTime={data.startAt}
+        toPath={"/virtual_live/" + data.id}
+        component={AgendaBox}
+      >
+        <AgendaPaper>
+          <Grid
+            container
+            alignItems="center"
+            spacing={2}
+            justifyContent="space-between"
           >
-            <Grid
-              container
-              columnSpacing={2}
-              alignItems="center"
-              sx={(theme) => ({
-                margin: theme.spacing("1%", 0),
-                padding: theme.spacing("2%", 0),
-              })}
-            >
-              <Grid item xs={12} md={4}>
-                <Image
-                  src={virtualLiveLogo}
-                  alt={getTranslated(`virtualLive_name:${data.id}`, data.name)}
-                  // aspectRatio={3.2562}
-                  bgColor=""
-                ></Image>
-              </Grid>
-              <Grid item xs={12} md={8}>
-                <Grid container direction="column" columnSpacing={1}>
-                  <Grid item>
-                    <SpoilerTag
-                      releaseTime={
-                        new Date(
-                          data.virtualLiveSchedules[0]
-                            ? data.virtualLiveSchedules[0].startAt
-                            : data.startAt
-                        )
-                      }
-                    />
+            <Grid item xs={12} md={4}>
+              <Image
+                src={virtualLiveLogo}
+                alt={getTranslated(`virtualLive_name:${data.id}`, data.name)}
+                // aspectRatio={3.2562}
+                bgColor=""
+              ></Image>
+            </Grid>
+            <Grid item xs={12} md={8}>
+              <Grid container direction="column" columnSpacing={1}>
+                <Grid item>
+                  <SpoilerTag
+                    releaseTime={
+                      new Date(
+                        data.virtualLiveSchedules[0]
+                          ? data.virtualLiveSchedules[0].startAt
+                          : data.startAt
+                      )
+                    }
+                  />
+                </Grid>
+                <Grid item>
+                  <ContentTrans
+                    contentKey={`virtualLive_name:${data.id}`}
+                    original={data.name}
+                    originalProps={{
+                      variant: "subtitle1",
+                    }}
+                  />
+                </Grid>
+                <Grid item container columnSpacing={1}>
+                  <Grid item xs={12}>
+                    <Typography variant="body2" color="textSecondary">
+                      {t(`virtual_live:type.${data.virtualLiveType}`)}
+                    </Typography>
                   </Grid>
                   <Grid item>
-                    <ContentTrans
-                      contentKey={`virtualLive_name:${data.id}`}
-                      original={data.name}
-                      originalProps={{
-                        variant: "subtitle1",
-                      }}
-                    />
+                    <Typography
+                      variant="body2"
+                      color="textSecondary"
+                      component="span"
+                    >
+                      {new Date(data.startAt).toLocaleString()}
+                    </Typography>
                   </Grid>
-                  <Grid item container columnSpacing={1}>
-                    <Grid item xs={12}>
-                      <Typography variant="body2" color="textSecondary">
-                        {t(`virtual_live:type.${data.virtualLiveType}`)}
-                      </Typography>
-                    </Grid>
-                    <Grid item>
-                      <Typography
-                        variant="body2"
-                        color="textSecondary"
-                        component="span"
-                      >
-                        {new Date(data.startAt).toLocaleString()}
-                      </Typography>
-                    </Grid>
-                    <Grid item>
-                      <Typography
-                        variant="body2"
-                        color="textSecondary"
-                        component="span"
-                      >
-                        ~
-                      </Typography>
-                    </Grid>
-                    <Grid item>
-                      <Typography
-                        variant="body2"
-                        color="textSecondary"
-                        component="span"
-                      >
-                        {new Date(data.endAt).toLocaleString()}
-                      </Typography>
-                    </Grid>
+                  <Grid item>
+                    <Typography
+                      variant="body2"
+                      color="textSecondary"
+                      component="span"
+                    >
+                      ~
+                    </Typography>
+                  </Grid>
+                  <Grid item>
+                    <Typography
+                      variant="body2"
+                      color="textSecondary"
+                      component="span"
+                    >
+                      {new Date(data.endAt).toLocaleString()}
+                    </Typography>
                   </Grid>
                 </Grid>
               </Grid>
             </Grid>
-          </SpoilerCard>
-        </Container>
-      </Link>
+          </Grid>
+        </AgendaPaper>
+      </SpoilerCard>
     );
   }
 );
